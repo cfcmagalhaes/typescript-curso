b8579117671110874d01512f41ac900bd27f4e2c
cfcmagalhaes <cfcmagalhaes@yahoo.com.br>
2021-12-06 00:46:37 -0300

    Finalizando parte 2/3

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -14,9 +14,9 @@ export class NegociacaoController {
     
 
     constructor(){
-        this.inputData = document.querySelector( '#data' );
-        this.inputQuantidade = document.querySelector( '#quantidade' );
-        this.inputValor = document.querySelector( '#valor' );
+        this.inputData = <HTMLInputElement> document.querySelector( '#data' );
+        this.inputQuantidade = <HTMLInputElement> document.querySelector( '#quantidade' );
+        this.inputValor = <HTMLInputElement> document.querySelector( '#valor' );
         this.negociacoesView.update(this.negociacoes);
     }
 
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,12 +1,10 @@
 export class Negociacoes {
     constructor() {
-        // Array<Negociacao> = Negociacao[]
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    // ReadonlyArray<Negociacao> = readonly Negociacao[]
     lista() {
         return this.negociacoes;
     }
